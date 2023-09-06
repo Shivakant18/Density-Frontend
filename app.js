c1cc680e9d3f6e39b8c651020e54f32e05526226
Shivakant18 <skm846187@gmail.com>
2023-09-06 19:55:01 +0530

    added animation using greensock

---
+++ app.js
@@ -0,0 +1,37 @@
+gsap.to(
+  ".circular-div",{
+        x:170,
+        duration:2,
+        delay:3
+  }
+)
+
+gsap.to(
+  "#heading1",{
+    scale:1.2
+  }
+)
+
+gsap.to(".card",{
+  x:-300,
+  duration:2,
+})
+
+gsap.to(".card",{
+  delay:2,
+  x:0,
+  duration:2,
+})
+
+gsap.to(".some-text-middle",{
+  scale:1.2,
+  delay:2
+})
+gsap.to(".some-text-right",{
+  scale:1.2,
+  delay:2
+})
+
+
+
+// Variables
